12ce3448bd4779cbec9ab97ec001ec7ae702bd02
vvfgg <139482629+vvfgg@users.noreply.github.com>
2024-07-15 13:34:28 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: d265b143-9a8a-4ade-83ab-5ecf3357f075,
 
   // basic site info (required)
   name: 'Jan自留地',
